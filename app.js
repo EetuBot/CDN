9a545f80ae7a486287e3329bc6b84e12b58e9286
Slayer <51095261+GhostSlayer@users.noreply.github.com>
2021-05-26 17:28:33 +0300

    Use base64 instead

--- app.js
+++ app.js
@@ -3,18 +3,25 @@ const app = express()
 const config = require('./config.json')
 const fs = require('fs')
 const helmet = require('helmet')
+const base64Img = require('base64-img');
 
 app.use(helmet())
 app.use(express.static('public'))
  
 app.get('/image/apina', function (req, res) {
   const kuvat = fs.readdirSync(__dirname + '/public/assets/images/apinat/')
-  const url = req.protocol + '://' + req.get('host');
-  return res.send({
-    success: true,
-    code: 200,
-    image: url + '/assets/images/apinat/' + kuvat[Math.floor(Math.random() * kuvat.length)]
-  })
+  const kuva = __dirname + '/public/assets/images/apinat/' + kuvat[Math.floor(Math.random() * kuvat.length)]
+
+  var imageData1 = base64Img.base64Sync(kuva);
+  var base64Data = imageData1.replace(/^data:image\/(png|jpeg|jpg);base64,/, '');
+  var img = Buffer.from(base64Data, 'base64');
+
+  res.writeHead(200, {
+    'Content-Type': 'image/png',
+    'Content-Length': img.length
+  });
+
+  res.end(img); 
 })
 
 app.get('*', (req, res) => {
